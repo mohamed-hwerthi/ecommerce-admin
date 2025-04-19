415b8f53f540684ddb1fa07bc2998661d7159b1c
mohamed <hwerthimohamed25@gmail.com>
2025-04-19 23:38:32 +0100

    update  category image

--- src/app/modules/admin/dashboard/components/category/category-update-modal/category-update-modal.component.ts
+++ src/app/modules/admin/dashboard/components/category/category-update-modal/category-update-modal.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
 import { FileUploadModule } from 'primeng/fileupload';
@@ -10,6 +11,7 @@ import { closeUpdatecategoryModal } from 'src/app/core/state/modal/category/moda
 import { selectCategoryToUpdate } from 'src/app/core/state/modal/category/modal.selector';
 import { MediaService } from 'src/app/modules/admin/layout/services/media.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: '[category-update-modal]',
   standalone: true,
@@ -18,9 +20,13 @@ import { CategoryService } from 'src/app/services/category.service';
   styleUrl: './category-update-modal.component.scss',
 })
 export class CategoryUpdateModalComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
   categoryForm: FormGroup;
   private currentCategoryId: number | null = null;
   selectedFile: File | null = null;
+  imagePreviewUrl: SafeUrl | null = null;
+  private existingMedia: Media[] = [];
 
   constructor(
     private readonly fb: FormBuilder,
@@ -28,6 +34,7 @@ export class CategoryUpdateModalComponent {
     private readonly catgoryService: CategoryService,
     private readonly toastr: ToastrService,
     private readonly mediaService: MediaService,
+    private readonly sanitizer: DomSanitizer,
   ) {
     // Initialize the form with structure
     this.categoryForm = this.fb.group({
@@ -46,16 +53,29 @@ export class CategoryUpdateModalComponent {
           name: category.name,
           description: category.description,
         });
+        this.existingMedia = category.medias || [];
+
+        this.imagePreviewUrl = this.sanitizer.bypassSecurityTrustUrl(this.getMenuItemImage(category));
       }
     });
+  }
+  getMenuItemImage(cateogry: CategoryDTO): string {
+    if (cateogry.medias.length > 0) {
+      return environment.apiStaticUrl + cateogry.medias[0].url;
+    } else {
+      return '';
+    }
   }
 
   handelCategoryUpdating() {
     if (this.categoryForm.valid) {
+      const formValue = this.categoryForm.value;
+
       if (this.selectedFile) {
-        this.uploadCategoryImageAndSaveCategory(this.categoryForm.value, this.selectedFile);
+        this.uploadCategoryImageAndSaveCategory(formValue, this.selectedFile);
       } else {
-        this.updateCategoryDirectly(this.categoryForm.value);
+        formValue.medias = this.existingMedia;
+        this.updateCategoryDirectly(formValue);
       }
     } else {
       this.categoryForm.markAllAsTouched();
@@ -79,10 +99,19 @@ export class CategoryUpdateModalComponent {
     this.store.dispatch(closeUpdatecategoryModal());
   }
   onFileSelected(event: any): void {
-    const file = event.files[0];
+    const file = event.target.files[0];
     if (file) {
       this.selectedFile = file;
+      this.generateImagePreview(file);
     }
+  }
+
+  private generateImagePreview(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imagePreviewUrl = this.sanitizer.bypassSecurityTrustUrl(e.target.result);
+    };
+    reader.readAsDataURL(file);
   }
   private uploadCategoryImageAndSaveCategory(submissionValues: any, file: File): void {
     this.mediaService.uploadFile(file).subscribe({
@@ -92,5 +121,13 @@ export class CategoryUpdateModalComponent {
       },
       error: (error: any) => this.toastr.error('Error uploading media file!', error?.message || 'Unknown error'),
     });
+  }
+  removeImage(): void {
+    this.selectedFile = null;
+    this.imagePreviewUrl = null;
+    this.categoryForm.get('media')?.setValue(''); /* todo : a voir quel est  son role */
+    if (this.fileInput) {
+      this.fileInput.nativeElement.value = '';
+    }
   }
 }
